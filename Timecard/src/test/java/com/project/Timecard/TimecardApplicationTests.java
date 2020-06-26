ac348938ceac7e8cbea440b4f6f49ddcbaafaf88
Patrick Du <dupatrick@gmail.com>
2020-06-26 13:06:11 -0400

    have to redo

---
+++ Timecard/src/test/java/com/project/Timecard/TimecardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Timecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimecardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
