5f6ea86ec319b47b2c7a4cda0cea516ea7ee9dc6
abhinavkannojia <abhinav.kannojia@1gmail.com>
2020-06-29 02:42:34 -0400

    New Changes

--- Timecard/src/main/java/com/summitworks/project/timecard/TimecardApplication.java
+++ Timecard/src/main/java/com/summitworks/project/timecard/TimecardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages={"com.summitworks.project.timecard"})
+@SpringBootApplication
 //@ComponentScan({"com.summitworks.project.timecard"})
 //@EntityScan("com.summitworks.project.timecard.model")
 //@EnableJpaRepositories("com.summitworks.project.timecard.repo")
